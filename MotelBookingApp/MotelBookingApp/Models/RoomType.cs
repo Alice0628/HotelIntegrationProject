9c8e030c889b89665772d06938d203dd61e77c98
Xiao Zhang <zxiao0628@gmail.com>
2023-02-20 20:05:50 -0500

    correct namespaces

--- MotelBookingApp/MotelBookingApp/Models/RoomType.cs
+++ MotelBookingApp/MotelBookingApp/Models/RoomType.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; } = default;
         public string Name { get; set; } = String.Empty;
         public string Price { get; set; } = String.Empty;
-        public string Description { get; set; } = String.Empty;
+        public string? Description { get; set; } = String.Empty;
         public string ImageUrl { get; set; } = String.Empty;
         public int Sleep { get; set; } = default;
         public int Amount { get; set; } = default;
