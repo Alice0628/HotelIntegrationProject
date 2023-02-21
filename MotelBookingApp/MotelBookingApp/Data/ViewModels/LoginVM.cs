33f097fe02d22005f358ae8269019ef3ee996306
Qichen Zhu <109364886+Electivire9@users.noreply.github.com>
2023-02-20 20:50:51 -0500

    push test new folder

---
+++ MotelBookingApp/MotelBookingApp/Data/ViewModels/LoginVM.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MotelBookingApp.Data.ViewModels
+{
+    public class LoginVM
+    {
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "Email address is required")]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
