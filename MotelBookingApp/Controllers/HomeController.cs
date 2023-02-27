08f520d854ec12f4e5b21e0b7ed5b16915ecfb29
Xiao Zhang <zxiao0628@gmail.com>
2023-02-27 18:50:33 -0500

    add homecontroller content

---
+++ MotelBookingApp/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MotelBookingApp.Data;
+using MotelBookingApp.Models;
+using MotelBookingApp.Data.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Identity;
+using System.Runtime.InteropServices;
+
+namespace MotelBookingApp.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private readonly MotelDbContext _context;
+        private readonly string _storageConnectionString;
+        private readonly string _storageContainerName;
+        private readonly BlobContainerClient _client;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly Microsoft.AspNetCore.Hosting.IHostingEnvironment _environment;
+
+        public HomeController(IConfiguration configuration, MotelDbContext context, UserManager<AppUser> userManager)
+        {
+            _context = context;
+            _storageConnectionString = configuration.GetValue<string>("BlobConnectionString");
+            _storageContainerName = configuration.GetValue<string>("BlobContainerName");
+            _userManager = userManager;
+            _client = new BlobContainerClient(_storageConnectionString, _storageContainerName);
+        }
+
+
+        // GET: AdminAirport
+        public async Task<IActionResult> Index()
+        {
+            return View();
+
+    }
+
+}
