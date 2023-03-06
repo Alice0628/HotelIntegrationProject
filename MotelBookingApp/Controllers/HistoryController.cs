28cd49ecb048e279e674c541837b8c5af014be57
Qichen Zhu <109364886+Electivire9@users.noreply.github.com>
2023-03-06 18:31:11 -0500

    booked history and home banner annimation

--- MotelBookingApp/Controllers/HistoryController.cs
+++ MotelBookingApp/Controllers/HistoryController.cs
@@ -24,8 +24,8 @@ namespace MotelBookingApp.Controllers
             _context = context;
         }
 
-        // Current user's BookedRecord history
-        public async Task<IActionResult> CustomerHistory(int? id, string searchString)
+        // BookedRecord history
+        public async Task<IActionResult> Detail(int? id, string searchString)
         {
             var userName = _userManager.GetUserName(User);
             var currentUser = _context.Users.Where(u => u.UserName == userName).FirstOrDefault();
@@ -37,34 +37,76 @@ namespace MotelBookingApp.Controllers
 
             if (_context.BookedRecords == null) return Problem("No purchases found.");
 
-            var purchases = await _context.BookedRecords
+            // Customer can view their own history
+            if (User.IsInRole("User"))
+            {
+                var purchases = await _context.BookedRecords
                 .Include(br => br.Room).ThenInclude(r => r.Motel)
                 .Include(br => br.Room).ThenInclude(r => r.RoomType)
                 .Include(br => br.Booking).ThenInclude(b => b.AppUser)
                 .Where(br => br.Booking.AppUser.Id == id).ToListAsync();
 
-            if (!String.IsNullOrEmpty(searchString))
-            {
-                searchString = searchString.ToLower();
-                purchases = await _context.BookedRecords.Where(br =>
-                    br.Booking.AppUser.FirstName.ToLower().Contains(searchString) ||
-                    br.Booking.AppUser.LastName.ToLower().Contains(searchString) ||
-                    br.Room.Motel.Name.ToLower().Contains(searchString) ||
-                    br.Room.RoomType.Name.ToLower().Contains(searchString) ||
-                    br.Booking.PayTime.ToString().Contains(searchString) ||
-                    br.CheckinDate.ToString().Contains(searchString) ||
-                    br.CheckoutDate.ToString().Contains(searchString)
-                    )
-                    .Where(br => br.Booking.AppUser.Id == id)
-                    .ToListAsync();
-
-                if (purchases.Count <= 0)
+                if (!String.IsNullOrEmpty(searchString))
                 {
-                    TempData["CHNotFound"] = "No related purchase Found!";
+                    searchString = searchString.ToLower();
+                    purchases = await _context.BookedRecords.Where(br =>
+                        br.Booking.AppUser.FirstName.ToLower().Contains(searchString) ||
+                        br.Booking.AppUser.LastName.ToLower().Contains(searchString) ||
+                        br.Room.Motel.Name.ToLower().Contains(searchString) ||
+                        br.Room.RoomType.Name.ToLower().Contains(searchString) ||
+                        br.Booking.PayTime.ToString().Contains(searchString) ||
+                        br.CheckinDate.ToString().Contains(searchString) ||
+                        br.CheckoutDate.ToString().Contains(searchString)
+                        )
+                        .Where(br => br.Booking.AppUser.Id == id)
+                        .ToListAsync();
+
+                    if (purchases.Count <= 0)
+                    {
+                        TempData["HistoryNotFound"] = "No related purchase Found!";
+                    }
                 }
+                ViewBag.Count = HttpContext.Session.GetString("Count");
+                return View(purchases);
             }
-            ViewBag.Count = HttpContext.Session.GetString("Count");
-            return View(purchases);
+
+            // Admin can view all histories
+            else if (User.IsInRole("Admin"))
+            {
+                var purchases = await _context.BookedRecords
+                .Include(br => br.Room).ThenInclude(r => r.Motel)
+                .Include(br => br.Room).ThenInclude(r => r.RoomType)
+                .Include(br => br.Booking).ThenInclude(b => b.AppUser).ToListAsync();
+
+                if (!String.IsNullOrEmpty(searchString))
+                {
+                    searchString = searchString.ToLower();
+                    purchases = await _context.BookedRecords.Where(br =>
+                        br.Booking.AppUser.FirstName.ToLower().Contains(searchString) ||
+                        br.Booking.AppUser.LastName.ToLower().Contains(searchString) ||
+                        br.Room.Motel.Name.ToLower().Contains(searchString) ||
+                        br.Room.RoomType.Name.ToLower().Contains(searchString) ||
+                        br.Booking.PayTime.ToString().Contains(searchString) ||
+                        br.CheckinDate.ToString().Contains(searchString) ||
+                        br.CheckoutDate.ToString().Contains(searchString)
+                        )
+                        .Where(br => br.Booking.AppUser.Id == id)
+                        .ToListAsync();
+
+                    if (purchases.Count <= 0)
+                    {
+                        TempData["HistoryNotFound"] = "No related purchase Found!";
+                    }
+                }
+                ViewBag.Count = HttpContext.Session.GetString("Count");
+                return View(purchases);
+            }
+
+            else
+            {
+                return RedirectToAction("AccessDenied", "Account");
+            }
+
         }
 
         // All PurchasedFlight 
